33dccaa81f9f4446d69ff1b7edb80d085b225975
Rcatarata <Rcatarata@gmail.com>
2024-02-02 19:25:39 -0500

    database set up

--- nextjs_sockets/app/layout.tsx
+++ nextjs_sockets/app/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 import { Open_Sans } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/providers/theme-provider';
+import { cn } from '@/lib/utils';
 
 const openSans = Open_Sans({ subsets: ['latin'] });
 
@@ -19,12 +20,12 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang='en' suppressHydrationWarning>
-        <body className={openSans.className}>
+        <body className={cn(openSans.className, 'bg-white dark:bg-[#313338]')}>
           <ThemeProvider
             attribute='class'
             defaultTheme='dark'
             enableSystem={false}
-            storageKey='themer'
+            storageKey='discord-theme'
           >
             {children}
           </ThemeProvider>
